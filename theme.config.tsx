e10f38bd81d4ee86ab8f0cfc36110254cf92f5da
iamkubi <iamkubi@gmail.com>
2024-03-23 17:01:12 -0700

    Base theme config

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Pelican Panel</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/pelican-dev/panel',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.com/pelican-panel',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/pelican-dev/docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Pelican Panel',
   },
 }
 
