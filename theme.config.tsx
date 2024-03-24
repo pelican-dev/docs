7b4ea263cd969325602f7d134a460f385f78eafa
AreYouScared <sir3lit@gmail.com>
2024-03-23 23:48:24 -0400

    Initial

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
+<<<<<<< Updated upstream
   logo: <span>Pelican Panel</span>,
   project: {
     link: 'https://github.com/pelican-dev/panel',
@@ -12,6 +13,18 @@ const config: DocsThemeConfig = {
   docsRepositoryBase: 'https://github.com/pelican-dev/docs',
   footer: {
     text: 'Pelican Panel',
+=======
+  logo: "Pelican",
+  project: {
+    link: 'https://github.com/pelican-dev/',
+  },
+  chat: {
+    link: 'https://discord.gg/pelican-panel/',
+  },
+  docsRepositoryBase: 'https://github.com/pelican-dev/docs',
+  footer: {
+    text: 'Pelican Panel <3',
+>>>>>>> Stashed changes
   },
 }
 
