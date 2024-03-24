f74ba76d7a0902d12d8f29181c52dc72c9e3f55e
Charles Morgan <sir3lit@gmail.com>
2024-03-23 23:51:36 -0400

    Update theme.config.tsx
    ... Gotta love conflicts...

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,18 +2,6 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-<<<<<<< Updated upstream
-  logo: <span>Pelican Panel</span>,
-  project: {
-    link: 'https://github.com/pelican-dev/panel',
-  },
-  chat: {
-    link: 'https://discord.com/pelican-panel',
-  },
-  docsRepositoryBase: 'https://github.com/pelican-dev/docs',
-  footer: {
-    text: 'Pelican Panel',
-=======
   logo: "Pelican",
   project: {
     link: 'https://github.com/pelican-dev/',
@@ -24,7 +12,7 @@ const config: DocsThemeConfig = {
   docsRepositoryBase: 'https://github.com/pelican-dev/docs',
   footer: {
     text: 'Pelican Panel <3',
->>>>>>> Stashed changes
+
   },
 }
 
