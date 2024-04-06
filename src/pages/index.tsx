d095f1e4f42b4aa2b8a8ce2585e4d52be0c36726
Lance Pioch <git@lance.sh>
2024-04-06 02:38:30 -0400

    Update home page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,7 +45,7 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`Home`}
       description="Caww-Kaww">
       <HomepageHeader />
       <main>
