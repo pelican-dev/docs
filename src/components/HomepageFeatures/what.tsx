d8cc92f49eeea6079a5dba7e59a98ad7403f3a47
notCharles <charles@pelican.dev>
2024-04-09 16:35:02 -0400

    add panel imgs

--- src/components/HomepageFeatures/what.tsx
+++ src/components/HomepageFeatures/what.tsx
@@ -9,27 +9,33 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Updated Interface',
-    img: 'img/1.png',},
+    title: 'Node Overview',
+    img: 'img/homepage/1.png',  },
+  {
+    title: 'User Overview',
+    img: 'img/homepage/2.png',  },
   {
     title: 'Create Node',
-    img: 'img/2.png',  },
+    img: 'img/homepage/3.png',  },
   {
-    title: 'Updated Interface',
-    img: 'img/3.png',  },
+    title: 'Editing an Egg',
+    img: 'img/homepage/4.png',  },
   {
-    title: 'Create Node',
-    img: 'img/4.png',  },
+    title: 'Editing a Server',
+    img: 'img/homepage/5.png',  },
+  {
+    title: 'Creating an API Key',
+    img: 'img/homepage/6.png',  },
 ];
 
 function Feature({title, img}: FeatureItem) {
   return (
-    <div className={clsx('col col--6')}>
+    <div className={clsx('col col--4')}>
       <div className="text--center">
-        <a href={img} ><img src={img} width='800px' height='360px'/></a>
-      </div>
-      <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
+      </div>
+      <div className="text--center" style={{paddingBottom: '15px'}}>
+        <a href={img} ><img src={img}/></a>
       </div>
     </div>
   );
@@ -40,6 +46,7 @@ export default function HomepageWhat(): JSX.Element {
     <section className={styles.features}>
       <div className="container">
       <h1 style={{textAlign: 'center'}}>What is Pelican?</h1>
+      <h5 style={{textAlign: 'center'}}>pre-release screenshots</h5>
         <div className="row">
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
