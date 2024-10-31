18124301e5fb6dde55d24e2041a8b5bb3427c4ba
Boy132 <Boy132@users.noreply.github.com>
2024-10-31 11:06:07 +0100

    navbar: change "install" to just docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -46,11 +46,7 @@ const config: Config = {
         src: "img/logo.png",
       },
       items: [
-         {
-          to: '/docs/panel/getting-started',
-          label: 'Install',
-          position: 'left'
-        },
+        { to: '/docs', label: 'Docs', position: 'left' },
         { to: "/blog", label: "Blog", position: "left" },
         { to: "/donate", label: "Donate", position: "left" },
         {
