a7716e4bfce363ba588902d193c85761a403b8fd
RMartinOscar <40749467+RMartinOscar@users.noreply.github.com>
2025-01-16 13:39:13 +0000

    Use eggsNest

--- src/components/HomepageFeatures/why.tsx
+++ src/components/HomepageFeatures/why.tsx
@@ -42,7 +42,7 @@ const FeatureList: FeatureItem[] = [
           <Link to={'https://www.valheimgame.com'}>Valheim</Link><>, </>
           <Link to={'https://enshrouded.com'}>Enshrouded</Link><>, </>
           <Link to={'https://factorio.com'}>Factorio</Link><>, </>
-          <Link to={'https://github.com/pelican-eggs'}>and so much more</Link>.
+          <Link to={'/eggsNest'} target={'_blank'}>and so much more</Link>.
         Check our eggs out, we like them scrambled!
       </>
     ),
