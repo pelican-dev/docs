c8b681fd60d71ccda5d29c301b5732a624d8c8fd
Charles <charles@pelican.dev>
2025-07-07 09:39:56 -0400

    remove old config, update config to suppress warnings

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -29,6 +29,7 @@ const config: Config = {
         },
         blog: {
           showReadingTime: true,
+          onUntruncatedBlogPosts: 'ignore',
         },
         theme: {
           customCss: [
